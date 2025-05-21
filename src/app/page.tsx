c306ef655ad167fbf78096851268c195f5b67de9
SAIFUL ISLAM (Fahim) <saifulislamfahim.me@gmail.com>
2025-05-21 23:14:38 +0600

    Remove the pop comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Division from "@/components/Home/Division";
 import HeroSection from "@/components/Home/HeroSection";
 import Navbar from "@/components/Shared/Navbar";
 
-//main page here
 export default function Home() {
   return (
     <main className="h-screen">
