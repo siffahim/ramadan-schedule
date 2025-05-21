9e0c3ee0d35e44f87d8fd90514bef60ee465a8cf
SAIFUL ISLAM (Fahim) <saifulislamfahim.me@gmail.com>
2025-05-21 23:10:43 +0600

    give a comment on main page
    Co-Authored-By: MD Nazmul Hasan  <105441794+mdnazmulhasanniloy@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Division from "@/components/Home/Division";
 import HeroSection from "@/components/Home/HeroSection";
 import Navbar from "@/components/Shared/Navbar";
 
+//main page here
 export default function Home() {
   return (
     <main className="h-screen">
